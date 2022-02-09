86e066ee7b108aa7624eb50610391c575b041149
aribuilds <ariamteclemichael@gmail.com>
2022-02-09 13:07:40 -0500

    fixed more errs

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,7 +18,7 @@ const validateCampground = (req, res, next) => {
 
 router.get('/', catchAsync(async (req, res) =>{
     const campgrounds = await Campground.find({});
-    res.render('/campgrounds/index', { campgrounds })
+    res.render('campgrounds/index', { campgrounds })
 }));
 
 router.get('/new', (req,res) =>{
@@ -39,7 +39,7 @@ router.get('/:id', catchAsync(async(req, res) =>{
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
     }
-    res.render('/campgrounds/show', {campground});
+    res.render('campgrounds/show', {campground});
 }))
 
 router.get('/:id/edit', catchAsync(async(req, res) =>{
