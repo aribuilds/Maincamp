7fa92b9f8131bc72d79bdf4efc7cbc6b56d80841
aribuilds <ariamteclemichael@gmail.com>
2022-03-22 13:17:05 -0400

    fixed img upload now works

--- models/campground.js
+++ models/campground.js
@@ -7,7 +7,7 @@ const Schema = mongoose.Schema;
   
 const CampgroundSchema = new Schema({
     title: String,
-    images: String,
+    image: String,
     price: Number,
     description: String,
     location: String,
